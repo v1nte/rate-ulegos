356d98959a42c5f801a398734c74ee5d76607765
v1nte <ivinte99@gmail.com>
2021-03-08 09:47:27 -0300

    Indenting

--- src/commponets/ProfessorCard.js
+++ src/commponets/ProfessorCard.js
@@ -5,12 +5,13 @@ import { Card } from 'react-bootstrap'
 export default function ProfessorCard({ Name, Img }){
   return (
     <Card className="bg-dark text-white" style={{ width: '45rem' }}>
-      <Card.Img variant="bottom" src={ Img } alt="asdfsdaf"/>
+      <Card.Img variant="top" src={ Img } alt=""/>
         <Card.Body>
           <Card.Title><h1>{ Name }</h1></Card.Title>
           <Card.Subtitle className="mb-2 text-muted">phD. de tu mamita</Card.Subtitle>
           <Card.Text> 
-          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</Card.Text>
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+          </Card.Text>
         </Card.Body>
       </Card>
   );
